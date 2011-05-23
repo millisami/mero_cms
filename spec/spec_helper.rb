f928006e95cde73aacdddb73ec34b3d8e04ae06a
Millisami <millisami@gmail.com>
2011-05-23 21:24:42 +0545

    database cleaner added to wipe out the database after each example run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment.rb", __FILE__)
 require 'rspec/rails'
+require "database_cleaner"
+
 Rails.backtrace_cleaner.remove_silencers!
 
 # Load support files
@@ -8,4 +10,19 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = false
+  
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    # DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+  
 end
