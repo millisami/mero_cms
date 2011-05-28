150aa7c1c93fa82c6595ef34ef4d92933bc4397c
Millisami <millisami@gmail.com>
2011-05-28 15:19:01 +0545

    page unit test green

--- app/models/mero_cms/page.rb
+++ app/models/mero_cms/page.rb
@@ -1,4 +1,9 @@
 module MeroCms
   class Page < ActiveRecord::Base
+    
+    validates :title, 
+              :presence => true,
+              :format => { :with => /^[a-zA-Z0-9\-,' ]+$/ }
+    validates :content, :presence => true
   end
 end
--- spec/models/mero_cms/page_spec.rb
+++ spec/models/mero_cms/page_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
-describe Page do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe MeroCms::Page do
+  
+  it { should have_valid(:title).when("About Us", "FAQ's") }
+  it { should_not have_valid(:title).when("123 $@^#\{\}", "") }
+  
+  it { should_not have_valid(:content).when("") }
+  
 end
